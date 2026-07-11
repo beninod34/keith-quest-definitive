43a986ed2394f42dd633b5d6e68ac6036e6d2b89
beninod34 <beninodorsey0304@gmail.com>
2026-07-11 19:45:18 -0400

    Add fancy badge plugin and make it work with v21

---
+++ Plugins/Fancy Badges/Config.rb
@@ -0,0 +1,24 @@
+#===============================================================================
+#  Fancy Badges
+#    by Luka S.J.
+#
+#  Renders a 3D version of a badge, and applies rotational animation
+#  to spice up those scenes when you obtain Gym Badges.
+#  Call it in an event via the 'script' command using:
+#      renderBadgeAnimation(X)
+#  Where "X" represents the internal ID of the badge
+#
+#  Enjoy the script, and make sure to give credit!
+#===============================================================================
+module FancyBadges
+  NAMES = [
+      "Megaman Cap",
+      "Forkless Pizza",
+      "Best Character Badge",
+      "Egg of Enlightenment",
+      "Durag of Greed",
+      "Keyblade",
+      "Shmoovement License",
+      "Eldritch Horrors Beyond Your Comprehension",
+  ] 
+end
